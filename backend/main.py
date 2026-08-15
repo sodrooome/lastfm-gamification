30e1fa1c58426c4ab8f5b20e41bdff01d922373c
Ryan <37971350+sodrooome@users.noreply.github.com>
2026-08-15 21:09:24 +0700

    fixed: change allow origins and set cname on every CI build (#22)

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ from _logging import configure_logging
 
 configure_logging()
 
-origins = ["https://sodrooome.github.io"]
+origins = ["https://tastecheck.me/"]
 
 if os.getenv("ENVIRONMENT") == "development":
     # extend allowed origins (just in case)
